cddb29a1a1429ea46e18c4b3fdaf59a293ef290c
Mihail Akimenko <mihails.akimenko@gmail.com>
2017-12-12 12:36:54 +0200

    Base project generated by Angular CLI (including minor fixes)

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('angular-template-for-dummies App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
