c971258ec79e75d995f2490578c4b4c6c529ac65
ComFreek <comfreek@outlook.com>
2018-05-15 12:44:34 +0200

    Add App component test for <canvas> existence

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,4 +28,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
+  it('should create <canvas> element', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('canvas')).not.toBeNull('No <canvas> rendered');
+  }));
 });
