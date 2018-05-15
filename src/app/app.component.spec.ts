1f82afb4fdfac03183e928627c175abd1f32552f
ComFreek <comfreek@outlook.com>
2018-05-15 12:43:47 +0200

    Fix missing imports in App component spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { ThreeWrapperModule } from 'three-wrapper';
+import { ThreeExamplesModule } from './three-examples/three-examples.module';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        FormsModule,
+        ThreeExamplesModule,
+        ThreeWrapperModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -17,11 +27,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
