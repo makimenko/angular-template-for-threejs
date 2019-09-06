5cf462910fbb0573f33e615afed2ac9594944d9e
Mihails Akimenko <mihails.akimenko@gmail.com>
2019-09-06 19:23:21 +0300

    Fixed imports

--- projects/atft/src/lib/atft.module.ts
+++ projects/atft/src/lib/atft.module.ts
@@ -1,29 +1,29 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
-import { OrbitControlsComponent } from './controls/orbit-controls.component';
-import { ObjLoaderComponent } from './objects/loaders/obj-loader.component';
-import { Rad2DegPipe } from './pipes/rad2deg.pipe';
-import { Deg2RadPipe } from './pipes/deg2rad.pipe';
-import { PerspectiveCameraComponent } from './cameras/perspective-camera.component';
-import { WebGLRendererComponent } from './renderer/webgl-renderer.component';
-import { SceneComponent } from './objects/scene.component';
-import { AxesHelperComponent } from './objects/helpers/axes-helper.component';
-import { GridHelperComponent } from './objects/helpers/grid-helper.component';
-import { ObjectLoaderComponent } from './objects/loaders/object-loader.component';
-import { PointLightComponent } from './objects/light/point-light.component';
-import { SphereMeshComponent } from './objects/mesh/sphere-mesh.component';
-import { CylinderMeshComponent } from './objects/mesh/cylinder-mesh.component';
-import { TorusMeshComponent } from './objects/mesh/torus-mesh.component';
-import { EmptyComponent } from './objects/helpers/empty.component';
-import { BoxMeshComponent } from './objects/mesh/box-mesh.component';
-import { TextMeshComponent } from './objects/text/text-mesh.component';
+import {OrbitControlsComponent} from './controls/orbit-controls.component';
+import {ObjLoaderComponent} from './objects/loaders/obj-loader.component';
+import {Rad2DegPipe} from './pipes/rad2deg.pipe';
+import {Deg2RadPipe} from './pipes/deg2rad.pipe';
+import {PerspectiveCameraComponent} from './cameras/perspective-camera.component';
+import {WebGLRendererComponent} from './renderer/webgl-renderer.component';
+import {SceneComponent} from './objects/scene.component';
+import {AxesHelperComponent} from './objects/helpers/axes-helper.component';
+import {GridHelperComponent} from './objects/helpers/grid-helper.component';
+import {ObjectLoaderComponent} from './objects/loaders/object-loader.component';
+import {PointLightComponent} from './objects/light/point-light.component';
+import {SphereMeshComponent} from './objects/mesh/sphere-mesh.component';
+import {CylinderMeshComponent} from './objects/mesh/cylinder-mesh.component';
+import {TorusMeshComponent} from './objects/mesh/torus-mesh.component';
+import {EmptyComponent} from './objects/helpers/empty.component';
+import {BoxMeshComponent} from './objects/mesh/box-mesh.component';
+import {TextMeshComponent} from './objects/text/text-mesh.component';
 import {MeshLineConnectorComponent} from './objects/connector/mesh-line-connector.component';
-import {LineConnectorComponent} from './objects/connector';
+import {LineConnectorComponent} from './objects/connector/line-connector.component';
 
+// NOTE: In case of "ERROR in Unexpected value 'undefined' exported by the module 'AtftModule" fix imports (do not import index.ts)
 
 @NgModule({
-  imports: [
-  ],
+  imports: [],
   declarations: [
     OrbitControlsComponent,
     ObjLoaderComponent,
@@ -44,7 +44,7 @@ import {LineConnectorComponent} from './objects/connector';
     TextMeshComponent,
     MeshLineConnectorComponent,
     LineConnectorComponent
-],
+  ],
   exports: [
     OrbitControlsComponent,
     ObjLoaderComponent,
@@ -67,4 +67,5 @@ import {LineConnectorComponent} from './objects/connector';
     LineConnectorComponent
   ]
 })
-export class AtftModule { }
+export class AtftModule {
+}
