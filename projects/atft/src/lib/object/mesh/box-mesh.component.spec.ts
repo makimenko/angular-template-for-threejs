0252ad97f8d729f8948e9f7853ba503118fc3c1b
makimenko <mihails.akimenko@gmail.com>
2019-09-25 13:14:17 +0300

    Build and test fixes

--- projects/atft/src/lib/object/mesh/box-mesh.component.spec.ts
+++ projects/atft/src/lib/object/mesh/box-mesh.component.spec.ts
@@ -1,9 +1,11 @@
-import {SphereMeshComponent} from './sphere-mesh.component';
-import {async, TestBed} from '@angular/core/testing';
+import {CylinderMeshComponent} from './cylinder-mesh.component';
 import {RendererService} from '../../renderer';
+import {async, TestBed} from '@angular/core/testing';
 import {AtftMeshModule} from './atft-mesh.module';
+import {FrameMeshComponent} from './frame-mesh.component';
+import {BoxMeshComponent} from './box-mesh.component';
 
-describe('SphereMeshComponent', () => {
+describe('BoxMeshComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,7 +20,7 @@ describe('SphereMeshComponent', () => {
   }));
 
   it('should create an instance', () => {
-    const fixture = TestBed.createComponent(SphereMeshComponent);
+    const fixture = TestBed.createComponent(BoxMeshComponent);
     const component = fixture.componentInstance;
     fixture.detectChanges();
     expect(component).toBeTruthy();
--- projects/atft/src/lib/object/mesh/plane-mesh.component.spec.ts
+++ projects/atft/src/lib/object/mesh/plane-mesh.component.spec.ts
@@ -1,9 +1,9 @@
-import {SphereMeshComponent} from './sphere-mesh.component';
 import {async, TestBed} from '@angular/core/testing';
 import {RendererService} from '../../renderer';
 import {AtftMeshModule} from './atft-mesh.module';
+import {PlaneMeshComponent} from './plane-mesh.component';
 
-describe('SphereMeshComponent', () => {
+describe('PlaneMeshComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,7 +18,7 @@ describe('SphereMeshComponent', () => {
   }));
 
   it('should create an instance', () => {
-    const fixture = TestBed.createComponent(SphereMeshComponent);
+    const fixture = TestBed.createComponent(PlaneMeshComponent);
     const component = fixture.componentInstance;
     fixture.detectChanges();
     expect(component).toBeTruthy();
