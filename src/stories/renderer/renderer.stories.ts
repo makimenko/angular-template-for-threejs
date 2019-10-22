5c24bba58358691c0586f94d6b761e03f1802097
Mihails Akimenko <mihails.akimenko@gmail.com>
2019-10-22 19:18:52 +0300

    Youtube minimal script (without dependencies)

--- src/stories/renderer/renderer.stories.ts
+++ src/stories/renderer/renderer.stories.ts
@@ -1,10 +1,9 @@
 import {moduleMetadata, storiesOf} from '@storybook/angular';
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 // NOTE: Do direct import instead of library (allows to watch component and easy to develop)
 import {AtftModule} from '../../../projects/atft/src/lib/atft.module';
 import {defaultSceneWrapper} from '../common/default-scene-wrapper';
 import {withKnobs} from '@storybook/addon-knobs';
-
 
 @Component({
   template: defaultSceneWrapper(`
@@ -21,6 +20,70 @@ class StorybookBoxMeshComponent {
 }
 
 
+@Component({
+  template: `
+      <div id="player"></div>
+
+  `
+})
+export class StorybookYoutubeComponent implements AfterViewInit, OnInit {
+  player: any;
+  private id = 'HSqxlbf3Z7M';
+
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+    if ((<any>window).YT) {
+      console.log('Re-using existing youtube script');
+    } else {
+      console.log('Attaching youtube script');
+      const doc = (<any>window).document;
+      const playerApiScript = doc.createElement('script');
+      playerApiScript.type = 'text/javascript';
+      playerApiScript.src = 'https://www.youtube.com/iframe_api';
+      doc.body.appendChild(playerApiScript);
+    }
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+
+    if ((<any>window).YT) {
+      console.log('YT already loaded');
+      this.createPlayer();
+    }
+    (<any>window).onYouTubeIframeAPIReady = () => {
+      console.log('*** onYouTubeIframeAPIReady');
+      this.createPlayer();
+    };
+  }
+
+  createPlayer() {
+    console.log('Create player');
+    this.player = new (<any>window).YT.Player('player', {
+      height: '100%',
+      width: '100%',
+      videoId: this.id,
+      playerVars: {
+        'autoplay': 1,
+        'rel': 0,
+        'controls': 0,
+        'loop': 1
+      },
+      events: {
+        'onReady': () => {
+          console.log('*** ON READY');
+        },
+        'onStateChange': (x) => {
+          console.log('*** ON STATE CHANGE: ', x);
+        }
+      }
+    });
+  }
+
+}
+
+
 storiesOf('Renderer', module)
   .addDecorator(withKnobs)
   .addDecorator(
@@ -33,6 +96,11 @@ storiesOf('Renderer', module)
   .add('css3d', () => ({
     component: StorybookBoxMeshComponent
   }))
+  .add('youtube', () => ({
+    component: StorybookYoutubeComponent
+  }))
+
+
 ;
 
 
