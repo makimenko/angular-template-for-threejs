475d959e9d917993483b5e4f4f38d45f9fd46701
Mihails Akimenko <mihails.akimenko@gmail.com>
2019-11-24 23:56:36 +0200

    Fix imports

--- src/stories/performance/performance.stories.ts
+++ src/stories/performance/performance.stories.ts
@@ -2,10 +2,37 @@ import {moduleMetadata, storiesOf} from '@storybook/angular';
 // NOTE: Do direct import instead of library (allows to watch component and easy to develop)
 import {AtftModule} from '../../../projects/atft/src/lib/atft.module';
 import {withKnobs} from '@storybook/addon-knobs';
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {performanceSceneWrapper} from '../scene-wrapper/performance-scene-wrapper';
-import {AbstractCameraRotation} from './abstract-camera-rotation';
 import {AnimationService} from '../../../projects/atft/src/lib/animation/animation.service';
+import {PerspectiveCameraComponent} from '../../../projects/atft/src/lib/camera';
+import * as THREE from 'three';
+
+
+abstract class AbstractCameraRotation {
+
+  @ViewChild(PerspectiveCameraComponent, {static: false})
+  camera;
+  clock = new THREE.Clock();
+  matrix = new THREE.Matrix4();
+  period = 20; // rotation time in seconds
+
+
+  constructor(protected animation: AnimationService) {
+    this.animate = this.animate.bind(this);
+    animation.animate.subscribe(this.animate);
+    animation.start();
+  }
+
+  animate() {
+    if (this.camera) {
+      this.matrix.makeRotationZ(this.clock.getDelta() * 2 * Math.PI / this.period);
+      this.camera.camera.position.applyMatrix4(this.matrix);
+      this.camera.camera.lookAt(new THREE.Vector3());
+    }
+  }
+
+}
 
 
 @Component({
