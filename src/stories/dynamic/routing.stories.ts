ea0c2c2031b61a08890c801ce1c25b8be14b3b4f
Mihails Akimenko <mihails.akimenko@gmail.com>
2020-10-06 22:39:29 +0300

    Dynamic route storybook

--- src/stories/dynamic/routing.stories.ts
+++ src/stories/dynamic/routing.stories.ts
@@ -1,43 +1,85 @@
-import {Component, NgModule} from '@angular/core';
+import {Component, NgModule, Optional, SkipSelf} from '@angular/core';
 import {moduleMetadata} from '@storybook/angular';
 // NOTE: Do direct import instead of library (allows to watch component and easy to develop)
 import {AtftModule} from '../../../projects/atft/src/lib/atft.module';
-import {RouterModule, Routes} from '@angular/router';
+import {Router, RouterModule, Routes} from '@angular/router';
 import {BrowserModule} from '@angular/platform-browser';
 import {APP_BASE_HREF} from '@angular/common';
+import {worldSceneWrapper} from '../scene-wrapper/world-scene-wrapper';
+import {EmptyComponent} from '../../../projects/atft/src/lib/object/helper';
+import {provideParent} from '../../../projects/atft/src/lib/util';
+import {RendererService} from '../../../projects/atft/src/lib/renderer';
+import {AbstractObject3D} from '../../../projects/atft/src/lib/object';
 
 
 @Component({
   selector: 'app-main',
-  template: `
-    <h1>Main</h1>
-    <input value="First" type="button" routerLink="first"/>
-    <input value="Second" type="button" routerLink="second"/>
-    <hr/>
-    <router-outlet></router-outlet>
-  `
+  template: worldSceneWrapper(`
+    <atft-empty name="main">
+      <atft-empty name="selector">
+        <atft-box-mesh name="box1" atft-raycaster-group (click)="chooseFirst()" height="20" width="20" depth="20" translateZ="10">
+        </atft-box-mesh>
+        <atft-box-mesh name="box2" atft-raycaster-group (click)="chooseSecond()" height="20" width="20" depth="20" translateZ="10" translateX="30">
+        </atft-box-mesh>
+      </atft-empty>
+      <router-outlet></router-outlet>
+    </atft-empty>
+`)
 })
 class MainComponent {
+
+  constructor(private router: Router) {
+  }
+
+  public chooseFirst() {
+    console.log('Choose first');
+    this.router.navigate(['first']);
+  }
+
+  public chooseSecond() {
+    console.log('Choose second');
+    this.router.navigate(['second']);
+  }
 
 }
 
 @Component({
   selector: 'app-first-page',
+  providers: [provideParent(FirstPageComponent)],
   template: `
-    <h2>First Page</h2>
+    <atft-box-mesh name="first-box" height="10" width="10" depth="10" translateZ="30">
+    </atft-box-mesh>
   `
 })
-class FirstPageComponent {
+class FirstPageComponent extends EmptyComponent {
+
+  constructor(
+    protected rendererService: RendererService,
+    @SkipSelf() @Optional() protected parent: AbstractObject3D<any>
+  ) {
+    super(rendererService, parent);
+    console.log('FirstPageComponent.constructor', parent);
+  }
 
 }
 
 @Component({
   selector: 'app-second-page',
+  providers: [provideParent(SecondPageComponent)],
   template: `
-    <h2>Second Page</h2>
+    <atft-box-mesh name="second-box" height="10" width="10" depth="10" translateZ="30" translateX="30">
+    </atft-box-mesh>
   `
 })
-class SecondPageComponent {
+class SecondPageComponent extends EmptyComponent {
+
+  constructor(
+    protected rendererService: RendererService,
+    @SkipSelf() @Optional() protected parent: AbstractObject3D<any>
+  ) {
+    super(rendererService, parent);
+    console.log('SecondPageComponent.constructor', parent);
+  }
 
 }
 
@@ -65,7 +107,8 @@ class StoryRoutingModule {
   ],
   imports: [
     BrowserModule,
-    StoryRoutingModule
+    StoryRoutingModule,
+    AtftModule
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [MainComponent]
@@ -79,7 +122,6 @@ export default {
   decorators: [
     moduleMetadata({
       imports: [
-        AtftModule,
         StoryModule
       ],
       declarations: []
