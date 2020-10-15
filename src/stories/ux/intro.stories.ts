704efd102891c942d36d67dd6faa72098bbf4263
makimenko <mihails.akimenko@gmail.com>
2020-10-15 15:52:21 +0300

    Intro scene orth

--- src/stories/ux/intro.stories.ts
+++ src/stories/ux/intro.stories.ts
@@ -7,35 +7,41 @@ import {EmptyComponent} from '../../../projects/atft/src/lib/object/helper';
 
 @Component({
   template: `
-    <atft-orbit-controls style="height:100%" rotateSpeed=1 zoomSpeed=1.2>
-      <atft-renderer-canvas>
-        <atft-orthographic-camera [positionX]=0 [positionY]=0 [positionZ]="1000" [zoom]="2">
-        </atft-orthographic-camera>
-        <atft-scene name="scene" background="0x002f6c">
-          <atft-point-light intensity="0.5" distance="1000" translateX=90 translateY=90
-                            translateZ=90></atft-point-light>
-          <atft-point-light intensity="0.8" distance="1000" [translateX]="-60" [translateY]="-60"
-                            [translateZ]="50"></atft-point-light>
-          <atft-axes-helper size=200></atft-axes-helper>
-          <atft-grid-helper [size]=200 [divisions]=10 [rotateX]="90 | deg2rad"></atft-grid-helper>
+    <atft-renderer-canvas>
+      <atft-orthographic-camera [positionX]=0 [positionY]=0 [positionZ]="1000" [zoom]="4">
+      </atft-orthographic-camera>
+      <atft-scene name="scene" background="0x000051">
+        <atft-ambient-light color="0xFFFFFF" intensity="0.4"></atft-ambient-light>
 
-          <atft-empty>
-            <atft-box-mesh *ngFor="let item of [].constructor(20); let i = index"
-                           height="10" width="10" depth="10" material="phong" materialColor="0xffffff"
-                           [translateY]="0" [translateZ]="0" [translateX]="(i*15)-150"></atft-box-mesh>
-          </atft-empty>
+        <atft-point-light intensity="0.5" distance="1000" translateX=90 translateY=90
+                          translateZ=90></atft-point-light>
+        <atft-point-light intensity="0.8" distance="1000" [translateX]="-60" [translateY]="-60"
+                          [translateZ]="50"></atft-point-light>
+        <atft-dof *ngIf="enable" [focus]="focus" [aperture]="aperture" [maxblur]="maxblur"></atft-dof>
 
+        <atft-empty translateZ="-1000">
           <atft-box-mesh *ngFor="let item of [].constructor(20); let i = index"
-                         height="10" width="10" depth="10" material="phong" materialColor="0xffffff"
+                         height="10" width="10" depth="10" material="phong" materialColor="0x5c6bc0"
                          [translateY]="0" [translateZ]="0" [translateX]="(i*15)-150"></atft-box-mesh>
+        </atft-empty>
 
+        <atft-text-mesh [text]="text" translateX="-100" translateY="-50" translateZ="100"
+                        materialColor="0x8e99f3" height="1" size="20"
+        ></atft-text-mesh>
 
-        </atft-scene>
-      </atft-renderer-canvas>
-    </atft-orbit-controls>
+      </atft-scene>
+    </atft-renderer-canvas>
   `
 })
 class StorybookIntroComponent implements AfterViewInit {
+
+  focus = 1.0;
+  aperture = 0.025;
+  maxblur = 0.01;
+  enable = true;
+
+  text = 'Welcome';
+
 
   @ViewChild(EmptyComponent) box;
 
@@ -52,8 +58,6 @@ class StorybookIntroComponent implements AfterViewInit {
 
   public animate() {
     this.k += 0.02;
-    this.box.rotateX = this.k;
-    this.box.rotateY = -this.k * 0.2;
     this.box.rotateZ = this.k * 3.3;
     this.box.applyRotation();
   }
