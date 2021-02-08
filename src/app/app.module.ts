d424ae82e0b11d58a2ecadaba1bbcaff5dbfe43f
Mihails Akimenko <mihails.akimenko@gmail.com>
2021-02-09 00:22:01 +0200

    editor: animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,10 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {EditorModule} from './editor/editor.module';
 
-
-import { AppComponent } from './app.component';
-import { EditorModule } from './editor/editor.module';
-
-// import { AtftModule } from 'atft';
-// TODO: For development (code watch):
-import { AtftModule } from 'projects/atft/src/lib/atft.module';
 
 @NgModule({
   declarations: [
--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -1,18 +1,15 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { AnimationService, RendererService, StatsService } from 'atft';
-import { AtftDataCenterActorModule } from '../../../projects/atft/src/lib/actor/data-center';
-import { AtftModule } from '../../../projects/atft/src/lib/atft.module';
-import { EditorCanvasComponent } from './editor-canvas/editor-canvas.component';
-import { EditorContentComponent } from './editor-content/editor-content.component';
-import { EditorLayoutComponent } from './editor-layout/editor-layout.component';
-import { EditorSideComponent } from './editor-side/editor-side.component';
-import { EditorToolbarComponent } from './editor-toolbar/editor-toolbar.component';
-
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {EditorCanvasComponent} from './editor-canvas/editor-canvas.component';
+import {EditorContentComponent} from './editor-content/editor-content.component';
+import {EditorLayoutComponent} from './editor-layout/editor-layout.component';
+import {EditorSideComponent} from './editor-side/editor-side.component';
+import {EditorToolbarComponent} from './editor-toolbar/editor-toolbar.component';
+import {AnimationService, AtftModule, RendererService, StatsService, AtftDataCenterActorModule} from 'atft';
 
 @NgModule({
   imports: [
