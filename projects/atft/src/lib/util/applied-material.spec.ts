eee5e47bb45ad8bf718b09891972c5b37abb741d
alexx855 <info@alexpedersen.net>
2021-04-15 23:43:47 +0200

    colors type fix

--- projects/atft/src/lib/util/applied-material.spec.ts
+++ projects/atft/src/lib/util/applied-material.spec.ts
@@ -5,24 +5,24 @@ describe('utils', () => {
   describe('appliedMaterial', () => {
 
     it('default', () => {
-      const material = appliedMaterial(0xff0000);
+      const material = appliedMaterial('0xff0000');
       expect(material.constructor).toBe(THREE.MeshPhongMaterial);
       expect(material.depthWrite).toBeTruthy();
     });
 
     it('basic', () => {
-      const material = appliedMaterial(0xff0000, 'basic');
+      const material = appliedMaterial('0xff0000', 'basic');
       expect(material.constructor).toBe(THREE.MeshBasicMaterial);
     });
 
     it('lambert', () => {
-      const material = appliedMaterial(0xff0000, 'lamb', true);
+      const material = appliedMaterial('0xff0000', 'lamb', true);
       expect(material.constructor).toBe(THREE.MeshLambertMaterial);
       expect(material.depthWrite).toBeTruthy();
     });
 
     it('lambert depth', () => {
-      const material = appliedMaterial(0xff0000, 'lamb', false);
+      const material = appliedMaterial('0xff0000', 'lamb', false);
       expect(material.constructor).toBe(THREE.MeshLambertMaterial);
       expect(material.depthWrite).toBeFalsy();
     });
