b82db939d4b07b45ffb94ad3303fb0c3e87977ce
Mihails Akimenko <mihails.akimenko@gmail.com>
2021-05-01 17:38:09 +0300

    #375: STL Loader

--- projects/atft/src/lib/object/loader/index.ts
+++ projects/atft/src/lib/object/loader/index.ts
@@ -2,5 +2,6 @@ export * from './atft-loader.module';
 export * from './obj-loader.component';
 export * from './object-loader.component';
 export * from './svg-loader.component';
+export * from './stl-loader.component';
 export * from './abstract-model-loader';
 export * from './services';
